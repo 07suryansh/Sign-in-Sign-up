20237bce414fbbbf3fa1ccf31f6d7a613c13d46e
Suryansh Singh Bisen <77867124+07suryansh@users.noreply.github.com>
2022-08-06 00:26:22 +0530

    added welcome message

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import "./App.css";
 import SIgnin from "./Components/SIgnin";
 import Signup from "./Components/Signup";
+import Welcome from "./Components/Welcome";
 
 function App() {
   return (
-    // <div className="App">
-    //   <div className="container">
-    //     <h1 className="heading">Sign In</h1>
-    // <div className="input1">
-    //   <input className="email" type="text" name="email" id="email" placeholder="Email"/>
-    // </div>
-    // <div className="input2">
-    //   <input className="password" type="password" name="password" id="password" placeholder="Password"/>
-    // </div>
-    // <div className="forgot">
-    // <a href="#">Forgot Password?</a>
-    // </div>
-    // <button type="button" className="btn btn-primary">Sign In</button>
-    //   </div>
-      
-    // </div>
+    
     <>
     {/* <SIgnin/> */}
-    <Signup/>
+    {/* <Signup/> */}
+    <Welcome/>
     </>
   );
 }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome() {
+  return (
+    <div className="wel">
+        <h1 className="display-1">Welcome</h1>
+        <hr className="border border-primary border-3 opacity-75"></hr>
+        <div type="submit" className="btn btn-primary">Let's Start</div>
+    </div>
+  )
+}
