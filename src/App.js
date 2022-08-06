30d224e53aeb8c8082f588abc674c7d016b99dfd
Suryansh Singh Bisen <77867124+07suryansh@users.noreply.github.com>
2022-08-06 09:14:43 +0530

    Added post message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import SignedIn from "./Components/SignedIn";
 import SIgnin from "./Components/SIgnin";
 import Signup from "./Components/Signup";
 import Welcome from "./Components/Welcome";
@@ -9,7 +10,8 @@ function App() {
     <>
     {/* <SIgnin/> */}
     {/* <Signup/> */}
-    <Welcome/>
+    {/* <Welcome/> */}
+    <SignedIn/>
     </>
   );
 }
--- src/Components/SIgnin.js
+++ src/Components/SIgnin.js
@@ -4,7 +4,8 @@ export default function SIgnin() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="heading">Sign In</h1>
+        <h1 className="heading1">Sign In</h1>
+        <form action="">
     <div className="input1">
       <input className="email" type="text" name="email" id="email" placeholder="Enter Email" required/>
     </div>
@@ -14,8 +15,11 @@ export default function SIgnin() {
     <div className="forgot">
     <a href="#">Forgot Password?</a>
     </div>
-    <button type="button" className="btn btn-primary">Sign In</button>
-    
+    <button type="submit" className="btn btn-primary">Sign In</button>
+    <div className="forgot">
+    <a href="#">Create Account</a>
+    </div>
+    </form>
       </div>
       
     </div>
